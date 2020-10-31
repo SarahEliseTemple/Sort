94e0889b1a4f3bbf5b776bc624f0820913213620
19717 <19717@192.168.1.132>
2020-10-30 22:15:51 -0700

    Second Video

--- Sort/src/Sort/Sort.java
+++ Sort/src/Sort/Sort.java
@@ -30,6 +30,7 @@ public class Sort {
 			ex.printStackTrace();
 			System.exit(0);
 		}
+		
 		String infile = fileInput.nextLine();
 		String[] InputStringArray = infile.split(",");
 		inputArray = new int[InputStringArray.length];
@@ -37,7 +38,40 @@ public class Sort {
 			inputArray[i] = Integer.parseInt(InputStringArray[i]);
 			System.out.println(inputArray[i]);
 		}
+		System.out.println("Enter a number for the sort you wish to use.");
+		System.out.println("1:Bubble Sort 2:Selection 3:Table 4:quicksort(dont use yet)");
+		input = consoleInput.nextLine();
+		if (input.length() != 1 && input.charAt(0) != '1' && input.charAt(0)!= '2' && 
+				input.charAt(0) != '3' && input.charAt(0) != '4') {
+			System.out.println("Enter 1, 2, 3, or 4");
+			while (input.length() != 1 && input.charAt(0) != '1' && input.charAt(0)!= '2' && 
+				input.charAt(0) != '3' && input.charAt(0) != '4') {
+			input = consoleInput.nextLine();
+			}
+		}
+		if (input.equals("1")) {
+			inputArray = bubbleSort(inputArray);
+		}
+		for (int i = 0; i < InputStringArray.length; i++) {
+			System.out.println(inputArray[i]);
+		}
 		
+	}
+	//compare each pair of numbers and move the larger to the right.  
+	int[] bubbleSort(int[] array) {
+		for (int j = 0; j < array.length; j++) {
+			
+		
+			for (int i = 0; i < array.length-1; i++) {
+				if(array[i] > array[i+1]) {
+					//swap
+					int temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+				}
+			}
+		}
+		return array;
 	}
 	
 	public static void main(String[] args) {
